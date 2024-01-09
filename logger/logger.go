6d05ca8c513699dbba6ea072296ec40bfbe53eb2
machengxue707 <machengxue707@gmail.com>
2024-01-09 10:09:53 +0800

    fix the time issue

--- logger/logger.go
+++ logger/logger.go
@@ -7,16 +7,25 @@ import (
 // print the time
 
 func init() {
-
+	// 设置全局logrus实例的格式化程序
 	logrus.SetFormatter(&logrus.TextFormatter{
-		FullTimestamp:   true, // Enable timestamp
+		FullTimestamp:   true,
 		TimestampFormat: "2006-01-02 15:04:05",
-		ForceColors:     true, // Force colored output even when stdout is not a terminal
+		ForceColors:     true,
 	})
-
 }
 
-var GLogger = logrus.New()
+var GLogger = createCustomLogger()
+
+func createCustomLogger() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		ForceColors:     true,
+	})
+	return logger
+}
 
 // var loggers = make(map[string]*logrus.Logger)
 
